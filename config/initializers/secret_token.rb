830a954d5adba7d53468cc8ad2669939eabd9f4e
Ben Abrams <ben@benabrams.com>
2012-08-05 17:21:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewMetropolis::Application.config.secret_token = 'a3e7538e09f0d9d5eaf2a2cbac96b55d6224157f8d21f64262fb40f86d06fee22a3995be58622b677aae4b0cbb10019aa2a3de3d8f5b2ad435bd96710a749749'
